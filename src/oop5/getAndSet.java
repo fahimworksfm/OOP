0a5fae620dae3557461fb386bae6d5a09f95fd37
fahimdotfm <fahimdotfm@fahims-mbp.lan>
2023-04-18 17:48:26 -0400

    Added getter and setter

---
+++ src/oop5/getAndSet.java
@@ -0,0 +1,16 @@
+package oop5;
+
+public class getAndSet {
+
+private String name;
+	
+	public String getName() {
+		
+		return name;
+	}
+	
+	public void setName(String myName) {
+		
+		this.name = myName;
+	}
+}
---
+++ src/oop5/runGetAndSet.java
@@ -0,0 +1,15 @@
+package oop5;
+
+
+public class runGetAndSet {
+
+public static void main(String[] args) {
+		
+		getAndSet run = new getAndSet();
+		
+		run.setName("Anick");
+		
+		System.out.println(run.getName());
+
+	}
+}
